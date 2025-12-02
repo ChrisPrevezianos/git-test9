f899c3e7aa358a172adcbebcf4162c0442765cb1
ChrisPrevezianos <chrisprevezianos@gmail.com>
2025-12-02 22:54:36 +0200

    feat: add divapp

--- src/gr/aueb/cf/testgit/DivApp.java
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -4,4 +4,9 @@ public class DivApp {
     public static void main(String[] args) {
 
     }
+
+    public static int divide(int a, int b){
+        if (b == 0) return 0;
+        return a / b;
+    }
 }
